e45ee6ed1f71bbeb8b64a9d0961131c1ce801e10
Eduardo Adão Locks <122515706+adao-locks@users.noreply.github.com>
2025-03-26 22:54:45 -0300

    time opens a popup to convert time, no function yet

--- src/app/component/page/time/time.component.ts
+++ src/app/component/page/time/time.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -7,6 +7,10 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrl: './time.component.css'
 })
 export class TimeComponent {
+  @ViewChild('tempo') tempoInput!: ElementRef;
+  @ViewChild('tempoEm') tempoEmSelect!: ElementRef;
+  @ViewChild('tempoPara') tempoParaSelect!: ElementRef;
+  resultado: string = '--';
 
   constructor(private router: Router, private route: ActivatedRoute) {}
 
@@ -17,4 +21,29 @@ export class TimeComponent {
   closePopup() {
     this.router.navigate([{outlets: {popup: null}}], {relativeTo: this.route.parent});
   }
+
+  converterTempo(event: Event): void {
+    event.preventDefault(); // Evita a atualização da página
+
+    const tempo = this.tempoInput.nativeElement.value;
+    const de = this.tempoEmSelect.nativeElement.value;
+    const para = this.tempoParaSelect.nativeElement.value;
+
+    const fatores: { [key: string]: number } = {
+      segundos: 1,
+      minutos: 60,
+      horas: 3600,
+      dias: 86400
+    };
+
+    if (!tempo || isNaN(tempo) || !fatores[de] || !fatores[para]) {
+      this.resultado = 'Entrada inválida';
+      return;
+    }
+
+    const tempoEmSegundos = tempo * fatores[de];
+    const resultadoConvertido = tempoEmSegundos / fatores[para];
+    this.resultado = `${resultadoConvertido} ${para}`;
+  }
+
 }
